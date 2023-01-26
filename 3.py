bc16faebf3d03b935001e417c54da0350534b6df
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-26 16:59:19 +0330

    second

---
+++ 3.py
@@ -0,0 +1,15 @@
+# number = 2
+# print(type(number))  # int       عدد صحیح          0  -1  2   1
+
+# number = 2.5  # float         اعشاری
+# print(type(number))
+
+
+x = 12
+y = 15
+
+z = x + y
+print(z)
+
+
+#  حاصل تفریق و تقسیم و ضرب دو عدد با لا را حساب کنید و نمایش دهید.
