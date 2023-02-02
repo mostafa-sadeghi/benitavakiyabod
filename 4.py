a4b03995f7702f0b02b8ac51269bb265e576643d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-02 17:01:49 +0330

    sadeghi

---
+++ 4.py
@@ -0,0 +1,41 @@
+# x = 1
+# print(type(x))
+# name = 'ali'
+# print(type(name))
+# name = "ali"
+# print(type(name))
+# نمایش کاراکتر های یک اسم
+# print(name)
+# print(name[0])
+# print(name[1])
+# print(name[2])
+
+# نمایش کاراکترهای اسم در کنار هم
+
+# name = "nikan"
+# print(name[0], name[1], name[2], name[3], name[4])
+# print(name[0], end='-')
+# print(name[1], end='-')
+# print(name[2], end='-')
+# print(name[3], end='-')
+
+# print(name[4])
+
+
+# slicing یا برش
+# print(name[:3])
+# print(name[0:3])
+
+# exercise 1:  string = 'abcdefghi'
+#  از رشته بالا برش های زیر را پرینت کنید:
+# abc          fgh         def        ghi      cde
+
+
+# message = 'he said "some thing"'
+# message = "he said \"some thing\""
+# message = '''he said "some thing"'''
+# print(message)
+
+
+message = "hello every body welcome to our python class.\nwe are going to learn every things about python. python from zero to hero!"
+print(message)
