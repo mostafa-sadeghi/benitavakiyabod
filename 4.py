9d1119314b1a2bde5738adcd0c0add360b7e730a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-09 17:01:06 +0330

    sadeghi

--- 4.py
+++ 4.py
@@ -37,5 +37,41 @@
 # print(message)
 
 
-message = "hello every body welcome to our python class.\nwe are going to learn every things about python. python from zero to hero!"
-print(message)
+# message = "hello every body welcome to our python class.\nwe are going to learn every things about python. python from zero to hero!"
+# message = """hello every body welcome to our python class.
+# we are going to learn every things about python. python from zero to hero!"""
+# message = '''hello every body welcome to our python class.
+# we are going to learn every things about python. python from zero to hero!'''
+# print(message)
+
+
+# name = input('enter your name: ')
+# print(type(name))
+# family = input('enter your family: ')
+# print(type(family))
+# message = "hello " + name + ' ' + family
+
+# print(message)
+
+# casting یا تبدیل نوع
+
+# x = int(input('enter first number: '))  # int('1')  int :  عدد صحیح
+# y = int(input('enter second number: '))  # int('1')
+
+# result = x + y
+
+# print("result is:", result)
+# x = float(input('enter first number: '))  # float('1.5')  float :  عدد اعشاری
+# y = float(input('enter second number: '))  # float('1.8')
+
+# result = x + y
+
+# print("result is:", result)
+
+
+# number1 + number2 - number3
+number1 = int(input('enter first number '))
+number2 = int(input('enter second number '))
+number3 = int(input('enter third number '))
+
+print(number1 + number2 - number3)
---
+++ about_string.py
@@ -0,0 +1,11 @@
+name = input("enter student's name ")
+course = input("enter course's name ")
+
+message = "hello " + name+" . welcome to our " + \
+    course+" class. "+course+" from zero to hero!"
+# formatted string
+message = f"hello {name}. welcome to our {course} class. {course} from zero to hero!"
+print(message)
+
+# exercise : برنامه ای بنویید که دو عدد را از کاربر در یافت نماید و حاصل جمع آن ها را به صورت زیر نمایش دهد
+#   2 + 3 = 5
