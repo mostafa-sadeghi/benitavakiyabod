e120d232441935821d66cb494591aef40d276b93
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-16 16:59:54 +0330

    sadehgi

---
+++ aboutString2.py
@@ -0,0 +1,26 @@
+# star = '*' * 10
+# print(star)
+
+# name = 'ali '
+
+# print(name * 10)
+shopping_list = []
+print("my shopping list before adding any item:", shopping_list)
+
+
+# print(type(shopping_list))
+shopping_list.append('egg')
+shopping_list.append('banana')
+
+print("my shopping list after adding items:", shopping_list)
+
+new_product = input('enter new product: ')
+
+shopping_list.append(new_product)
+
+print("my product list after adding new item:", shopping_list)
+
+numbers = []
+numbers.append(1)
+numbers.append(2)
+print(numbers)
