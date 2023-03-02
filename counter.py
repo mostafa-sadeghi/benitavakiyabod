cb6e7665b7a73eda0f357b88605be154a611191f
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-02 16:58:25 +0330

    sadeghi

--- counter.py
+++ counter.py
@@ -37,10 +37,19 @@ from fractions import Fraction
 # print(x)
 
 # exercise 1:
-# برنامه بنویسید که اسم یک دانش آموز را از وورودی دریافت نماید و
+# برنامه بنویسید که اسم یک دانش آموز را از ورودی دریافت نماید و
 # نمرات سه درس پایتون، اسکرچ و اپ اینونتر را از ورودی بگیرد و  معدل او را محاسبه و نمایش دهد
 # معدل یعنی مجموع نمرات تقسیم بر تعداشان
 # ali's average is :
 
 # x = 555/Fraction(1, 5)/5
 # print(x)
+
+# name = input("What's your name? ")
+# python_score = float(input('enter your python score: '))
+# scratch_score = float(input('enter your scratch score: '))
+# appInventor_score = float(input('enter your appInventor score: '))
+# average = (python_score + scratch_score + appInventor_score) / 3
+# print(name + "'s", "average is:", average)
+# print("%s's average is: %s" % (name, average))
+# print(f"{name}'s average is: {average:.1f}")
---
+++ list3.py
@@ -0,0 +1,54 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# numbers.append(100)
+# print("my list items:", numbers)
+
+# print("we want to delete last item:")
+# del numbers[9]
+# print("my list items:", numbers)
+
+# del numbers[-1]
+# print("my list items:", numbers)
+
+# print("we want to remove number 4 from the list")
+# numbers.remove(4)
+# print("my list items:", numbers)
+
+# print("we want to print our list reversely")
+# print(numbers[-1::-1])
+# del numbers[0]
+# del numbers[-1:0:-1]
+# print(numbers)
+
+# del numbers[-1:-4:-1]
+# print(numbers)
+# exericse 1:
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# برنامه ای بنویسید که مجموع اعداد زوج لیست بالا را پرینت نماید
+# برنامه ای  بنویسید که مجموع اعداد فرد لیست بالا را پرینت نماید
+
+
+# str1 = "ali"
+# str2 = "sima"
+
+# output = str1 + str2
+# print(output)
+
+
+# my_list1 = [1, 2, 3, 4, 5, 'a', 'b', 'c']
+# my_list2 = ["ali", "reza"]
+# output = my_list1 + my_list2
+# print(output)
+
+another_list = [1, 2, 3, [4, 5]]
+# print(another_list[0])
+# print(another_list[1])
+# print(another_list[2])
+# print(another_list[3])
+print("we want to print number 4 and number 5")
+
+print(another_list[3][0])
+print(another_list[3][1])
+
+# exercise 2   برنامه ای بنویسید که حاصل جمع اعداد موجود در لیست زیر ر امحاسبه نماید و پرینت کند
+somrthing = [1, 2, 3, [2, 2], [3, 6, 8]]
