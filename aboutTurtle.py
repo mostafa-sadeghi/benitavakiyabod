5a7fb2e27a8de6909ac2937709989658b124d787
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-06 16:59:55 +0330

    sadeghi

--- aboutTurtle.py
+++ aboutTurtle.py
@@ -2,19 +2,72 @@ import turtle
 
 s = turtle.Screen()
 s.bgcolor('orange')
+# s.bgpic('mario.png')
+s.register_shape('strawberry.gif')
 
 p = turtle.Pen()
-p.pencolor('red')
-p.pensize(4)
+p.pensize(2)
+p.pencolor("cyan")
+p.fillcolor("red")
+p.seth(45)
+p.ht()
+p.begin_fill()
+for i in range(10):
+    p.fd(2)
+    p.rt(6)
+print(p.heading())
+# p.seth(85)
+for i in range(18):
+    p.fd(2)
+    p.rt(6)
 
-p.forward(100)
-p.left(120)
+for i in range(25):
+    p.fd(2.12)
+p.penup()
+p.home()
+p.showturtle()
 
-p.forward(100)
-p.left(120)
 
-p.forward(100)
-p.left(120)
+p.seth(140)
+# p.ht()
+p.pendown()
+for i in range(10):
+    p.fd(2)
+    p.lt(6)
+print(p.heading())
+# # p.seth(85)
+for i in range(18):
+    p.fd(2)
+    p.lt(6)
+
+for i in range(25):
+    p.fd(2.12)
+
+p.end_fill()
+
+
+# shapes: 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'.
+# p.shape('turtle')
+# p.shape('strawberry.gif')
+
+# p.penup()
+# p.goto(100, 100)
+# p.stamp()
+
+# p.shape('turtle')
+# p.home()
+# p.shapesize(3)
+# p.pencolor('red')
+# p.pensize(4)
+
+# p.forward(100)
+# p.left(120)
+
+# p.forward(100)
+# p.left(120)
+
+# p.forward(100)
+# p.left(120)
 
 s.exitonclick()
 # exercise : کشیدن مربع، پنجضلعی و شش ضلعی
---
+++ about_if.py
@@ -0,0 +1,43 @@
+a = 5
+b = 5
+
+# if a < b:
+#     print("a < b")
+#     print("a is less than b")
+#     print("balalalla")
+
+# print("outside")
+
+# if a > b:
+#     print("a > b")
+#     print("a is greater than b")
+
+# print("done")
+
+
+# a = 5
+# b = 5
+# if a <= b:
+#     print("a is less than or equal to b")
+
+# if a >= b:
+#     print("a is greater than or equal to b.")
+
+
+# if a <= b:
+#     print("a is less than or equal to b")
+
+# else:
+#     print("a is greater than or equal to b.")
+# if a <= b:
+#     print("a is less than or equal to b")
+
+# elif a >= b:
+#     print("a is greater than or equal to b.")
+
+
+# تمرین
+'''
+برنامه ای بنویسید که دو عدد از ورودی دریافت نماید و عدد بزرگتر را نمایش دهد.
+
+'''
