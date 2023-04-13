e4a8958ad44b3e95bc8d8f7324b7c111215d9e7a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-13 17:00:44 +0330

    sadeghi

--- about_if.py
+++ about_if.py
@@ -1,5 +1,5 @@
-a = 5
-b = 5
+# a = 5
+# b = 5
 
 # if a < b:
 #     print("a < b")
@@ -41,3 +41,113 @@ b = 5
 برنامه ای بنویسید که دو عدد از ورودی دریافت نماید و عدد بزرگتر را نمایش دهد.
 
 '''
+
+# number1 = int(input('enter a number: '))
+# number2 = int(input('enter a number: '))
+
+# if number1 > number2:
+#     print("The greatest is:",number1)
+# else:
+#     print("The greatest is:",number2)
+
+# number1 = int(input('enter a number: '))
+# number2 = int(input('enter a number: '))
+# number3 = int(input('enter a number: '))
+
+
+# if number1 > number2 and number1 > number3:
+#     print("The greatest is:",number1)
+# elif number2 > number1 and number2 > number3:
+#     print("The greatest is:",number2)
+# else:
+#     print("The greatest is:",number3)
+
+# <   >    <=     >=     ==    !=
+
+# a = 4
+# b = 5
+
+# if a == b:
+#     print("a and b are equal")
+#     print("another")
+
+# if a != b:
+#     print("a and b are not equal")
+
+
+# a = 2
+# b = 3
+# c = 1
+
+# if a > b and a > c:
+#     print("a is greater than b and c") 
+
+# if a < b or a < c:
+#     print('a is less than b and c')
+
+# a = 2
+# b = 3
+# c = 1
+
+# maximum = a
+
+# if b > maximum:
+#     maximum = b
+# if c > maximum:
+#     maximum = c
+
+# print("max is:", maximum)
+
+
+
+
+
+# if a > b:
+#     if a > c:
+#         print("a is the greatest")
+# else:
+#     if b > c:
+#         print("b is the greatest")
+#     else:
+#         print("c is the greatest")
+
+
+# boolean
+True
+False
+
+# a = True
+# if a:
+#     print("yes")
+
+
+# b = False
+# if not b:
+#     print("yes")
+
+# exercise 1 :   
+'''
+برنامه ای بنویسید که نام دانش آموز و نمرات سه درس او را از ورودی دریافت نماید
+معدل دانش آموز را محاسبه نمائید
+و اگر معدل او از 
+90 
+بالاتر بود
+A را پرینت نماید
+
+> 80     B
+> 70     C
+> 60     D
+> 50     F
+
+'''
+
+
+# user_name = input('enter user name: ')
+
+# if user_name == 'admin' or user_name == 'root':
+#     print("login success.")
+#     print('user is valid.')
+
+# else:
+#     print("you can`t access this page.")
+#     print("permission denied.")
---
+++ s11.py
@@ -0,0 +1,28 @@
+# for i in range(5):
+#     print("I want to learn pygame.")
+#     print("I want to learn kivy.")
+# print("outside")
+
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(2,11,2):
+#     print(i)
+
+# exercise 2 :  با کمک حلقه فور اعداد فرد کمتر از ده را پرینت نمائید
+
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for s in [2,4,5,7,1]:
+#     print(s)
+
+# for s in 'python':
+#     print(s)
+
+# با کمک حلقه فور کاراکترهای اسم خودتان جداگانه در هر خط پرینت نمائید
