ee304074fcbe9bc4e878807e514f316c850a07f2
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-20 16:56:39 +0330

    sadeghi

---
+++ s12.py
@@ -0,0 +1,22 @@
+# for number in [1,5,0,-1,100]:
+#     print(number, end=' ')
+
+# for char in 'python':
+#     print(char.upper(), end=' ')
+# exe1:
+# برنامه ای بنویسید که لیستی از اعداد زیر بسازد و سپس مجموع آنها را محاسبه و نمایش دهد
+# 3 4 7 -1 0 9 99 103
+# with for loop
+
+total = 0
+for num in range(1000,1_000_001):
+    total = total + num
+
+print(total)
+
+
+
+
+# for i in range(3):
+#     print("a")
+
