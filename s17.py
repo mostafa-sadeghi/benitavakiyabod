5079e771265b14995e2d6958aaff89c6338db20d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-25 17:01:01 +0330

    sadeghi

---
+++ s17.py
@@ -0,0 +1,41 @@
+import turtle
+my_screen = turtle.Screen()
+pen = turtle.Pen()
+pen.shape('turtle')
+pen.color('brown', 'orange')
+pen.shapesize(2)
+pen.pensize(9)
+pen.seth(90)
+
+pen.forward(100)
+pen2 = pen.clone()
+pen.seth(0)
+pen2.setheading(180)
+
+for i in range(200):
+    pen.left(1)
+    pen2.right(1)
+    pen.forward(1)
+    pen2.forward(1)
+
+# pen.forward(100)
+# # pen.backward(100)
+
+# pen.stamp()
+
+# pen.color('green','red')
+
+# pen.goto(-200, -200)
+
+# pen2 = pen.clone()
+# pen2.color('gray','blue')
+
+# pen.home()
+
+# pen2.setpos(300,300)
+
+turtle.done()
+
+
+#  کشیدن چهار مربع rorated
+# توسط چهار clone
